4e47c9b0a06281c812f1d45280b2a66f90c8804a
hangh1022 <hangh1022@gmail.com>
2025-11-28 09:22:23 +0900

    문의 수정시 이미지도 수정 할 수 있게 수정

--- src/main/java/com/boot/dao/ComplainAttachDAO.java
+++ src/main/java/com/boot/dao/ComplainAttachDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Mapper
 public interface ComplainAttachDAO {
     public void insert(ComplainAttachDTO dto);
+    public void delete(String uuid); // uuid로 삭제하는 메서드 추가
     public List<ComplainAttachDTO> findByReportId(int report_id);
     public void deleteAll(int report_id);
     public ComplainAttachDTO findByUuid(String uuid); // 추가
